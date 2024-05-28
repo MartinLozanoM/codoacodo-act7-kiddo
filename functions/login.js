564ff5e5fa77ca0cd04b0fffc55c431b74768168
MartinLM <martinlozanomu@gmail.com>
2024-05-28 19:30:26 -0300

    Cree la pagina de login, un form login y un form register con validaciones

---
+++ functions/login.js
@@ -0,0 +1,20 @@
+const showHiddenPass = (loginPass, loginEye) => {
+    const input = document.getElementById(loginPass),
+    iconEye = document.getElementById(loginEye);
+
+    iconEye.addEventListener("click", () => {
+        if(input.type === "password"){
+            input.type = "text";
+
+            iconEye.classList.add("fa-eye");
+            iconEye.classList.remove("fa-eye-slash");
+        } else {
+            input.type = "password";
+            
+            iconEye.classList.remove("fa-eye");
+            iconEye.classList.add("fa-eye-slash");
+        }
+    })
+};
+
+showHiddenPass("login-pass","login-eye");
